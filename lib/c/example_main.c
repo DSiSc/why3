cad14abf7a1695512bf4c8d41f722f301391da44
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-08-29 12:53:33 +0200

    Update the example main

--- lib/c/example_main.c
+++ lib/c/example_main.c
@@ -1,3 +1,7 @@
+#include "why3.c"
+#include "why3__Tuple0.c"
+#include "demo__Demo.c"
+
 static void *_GC_realloc_(void *old, size_t unused, size_t new_size) {
     return GC_realloc(old, new_size);
 }
@@ -6,12 +10,12 @@ static void _GC_free_(void *old, size_t unused) {
     GC_free(old);
 }
 
-extern struct closure* M_test__Test__fact_rec;
+extern struct closure* M_demo__Demo__fact;
 
 int main(int argc, char** argv) {
     GC_init();
     mp_set_memory_functions(GC_malloc, _GC_realloc_, _GC_free_);
-    struct closure* main = M_test__Test__fact_rec;
+    struct closure* main = M_demo__Demo__fact;
     value (*f)(value, value*) = main->f;
     mpz_t lol;
     mpz_init_set_str(lol, argv[1], 10);
